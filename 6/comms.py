0a0e73b5a45e5d997abccfd9da0da2be717a3280
Tom Last <tom.last@five.ai>
2022-12-06 09:04:08 +0000

    Day 6

--- 6/comms.py
+++ 6/comms.py
@@ -2,6 +2,6 @@ with open("input") as file:
     full_comm_string = file.readline()
 
 for x in range(0, len(full_comm_string)):
-    if len(full_comm_string[x:x+14:1]) == len(set(full_comm_string[x:x+14:1])):
+    if len(set(full_comm_string[x:x+14:1])) == 14:
         print(f"point of start message is {x+14}")
         break
